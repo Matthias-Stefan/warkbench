9e5aba238e9301b1826a7a9bd94fcd0020d1a48e
Matthias-Stefan <matthias.stefan@private34.com>
2026-01-20 19:28:12 +0100

    project selection

--- src/editors/warkbench.editors.project_explorer/ViewModel/ProjectExplorerViewModel.cs
+++ src/editors/warkbench.editors.project_explorer/ViewModel/ProjectExplorerViewModel.cs
@@ -7,7 +7,7 @@ using warkbench.src.editors.core.ViewModel;
 
 namespace warkbench.src.editors.project_explorer.ViewModel;
 
-public class ProjectExplorerViewModel : Tool
+public class ProjectExplorerViewModel : Tool, IDisposable
 {
     public ProjectExplorerViewModel(
         IProjectService projectService, 
@@ -27,22 +27,72 @@ public class ProjectExplorerViewModel : Tool
         Properties = new TreeNodeViewModel("Properties", null);
         
         Assets = [ Projects, Worlds, Packages, Blueprints, Properties ];
+
+        _projectSelectionService.SelectionChanged += OnProjectSelectionChanged;
+        _worldSelectionService.SelectionChanged += OnWorldSelectionChanged;
+    }
+    
+    public void Dispose()
+    {
+        _projectSelectionService.SelectionChanged -= OnProjectSelectionChanged;
+        _worldSelectionService.SelectionChanged -= OnWorldSelectionChanged;
     }
 
+    private void OnProjectSelectionChanged(object? sender, SelectionChangedEventArgs<IProject> e)
+    {
+        SelectedProject = e.CurrentPrimary;
+    }
 
-
-
+    private void OnWorldSelectionChanged(object? sender, SelectionChangedEventArgs<IWorld> e)
+    {
+        SelectedWorld = e.CurrentPrimary;
+    }
+    
     public ITreeNode Projects { get; }
     public ITreeNode Worlds { get; }
     public ITreeNode Packages { get; }
     public ITreeNode Blueprints { get; }
     public ITreeNode Properties { get; }
-
+    
     public ObservableCollection<ITreeNode> Assets { get; init; }
 
+    public IProject? SelectedProject
+    {
+        get => _selectedProject;
+        set
+        {
+            if (SelectedProject is not null)
+                _projectSelectionService.Deselect(SelectedProject);
+            
+            if (value is null)
+            {
+                _projectService.SaveProject();
+                _projectService.CloseProject();
+                return;
+            }
+            
+            // new selection
+            _selectedProject = value;
+            _projectService.LoadProject(value.LocalPath);
+            _projectSelectionService.Select(value);
+        }
+    }
+
+    public IWorld? SelectedWorld
+    {
+        get => _selectedWorld;
+        set
+        {
+            
+        }
+    }
+    
     private readonly IProjectService _projectService;
     private readonly IWorldService _worldService;
     
     private readonly ISelectionService<IProject> _projectSelectionService;
     private readonly ISelectionService<IWorld> _worldSelectionService;
+
+    private IProject? _selectedProject;
+    private IWorld? _selectedWorld;
 }
